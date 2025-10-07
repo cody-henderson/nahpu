f80367f9a1bfe13fbbcb003cbcbc8766f00dc91d
Heru Handika <herubiolog@gmail.com>
2025-10-06 22:12:37 -0400

    Fix unnecessary this.

--- lib/services/utility_services.dart
+++ lib/services/utility_services.dart
@@ -32,20 +32,19 @@ String getSystemDateTime() {
 
 extension TimeOfDayFormatter on TimeOfDay {
   String toTimeStd() {
-    final hour = this.hour.toString().padLeft(2,'0');
-    final minute = this.minute.toString().padLeft(2,'0');
+    final hour = this.hour.toString().padLeft(2, '0');
+    final minute = this.minute.toString().padLeft(2, '0');
     final second = '00';
     return '$hour:$minute:$second';
   }
 
   String toTimeDisplay() {
-    final hour = this.hourOfPeriod.toString();
-    final minute = this.minute.toString().padLeft(2,'0');
-    final amPM = this.period.name.toUpperCase();
+    final hour = hourOfPeriod.toString();
+    final minute = this.minute.toString().padLeft(2, '0');
+    final amPM = period.name.toUpperCase();
     return '$hour:$minute $amPM';
   }
 }
-
 
 // Given a datetime, format it for display to the user (yMMMd)
 String dateTimeToDateDisplay(DateTime? inputDateTime) {
