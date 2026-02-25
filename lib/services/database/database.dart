aa1b77083c35f82a194c6accfd6e770b9dc2ec2b
Heru Handika <herubiolog@gmail.com>
2026-02-25 15:01:27 -0500

    Fix ignore.

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -152,13 +152,10 @@ class Database extends _$Database {
     await m.renameColumn(associatedData, 'secondaryId', associatedData.name);
     await m.renameColumn(associatedData, 'fileId', associatedData.url);
     // Remove secondaryIdRef
-    // ignore: experimental_member_use
     await m.alterTable(TableMigration(associatedData));
 
     // Sites
-    // ignore: experimental_member_use
     await m.alterTable(TableMigration(coordinate));
-    // ignore: experimental_member_use
     await m.alterTable(TableMigration(coordinate, columnTransformer: {
       coordinate.elevationInMeter: coordinate.elevationInMeter.cast<double>(),
     }));
@@ -183,9 +180,8 @@ class Database extends _$Database {
     await m.deleteTable('fileMetadata');
     await m.deleteTable('personnelPhoto');
     // delete column from media table and personnel tables
-    // ignore: experimental_member_use
+
     await m.alterTable(TableMigration(personnel));
-    // ignore: experimental_member_use
     await m.alterTable(TableMigration(media));
   }
 
